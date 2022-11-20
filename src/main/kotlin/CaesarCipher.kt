6b979f64461659c7c4a0fac62a37db4dfd7faddd
Do Hoon <dohun10170@gmail.com>
2022-11-20 18:08:05 +0900

    Rename

--- src/main/kotlin/CaesarCipher.kt
+++ src/main/kotlin/CaesarCipher.kt
@@ -1,5 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=kotlin
-class Solution {
+class CaesarCipher {
     private val alphabetCounts = 26
 
     fun solution(s : String, n : Int): String {
